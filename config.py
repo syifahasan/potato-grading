ffc3dc063cf0ab73bf272da94bfcd9ef534568d9
SyifaHasan <zakunime@gmail.com>
2026-06-24 11:38:57 +0700

    fix: moving terminal run codes to scripts folder

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ TEST_DIR = os.path.join(ROOT, "datasets","test", "images")
 MODEL_DIR = os.path.join(ROOT, "models", "weights")
 BEST_MODEL = os.path.join(MODEL_DIR, "best.pt")
 
-CALIB_PATH = os.path.join(ROOT, "src", "calibration.json")
+CALIB_PATH = os.path.join(ROOT, "scripts", "calibration.json")
 OUTPUT_DIR = os.path.join(ROOT, "outputs")
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
